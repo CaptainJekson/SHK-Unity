e30594ac2512b1aef09cd3ce991304efc0f0b92e
Skvorcov Evgeniy <creeger777@gmail.com>
2020-01-26 00:34:33 +0300

    Refactoring v1.4

--- Assets/Group1/Scripts/Player.cs
+++ Assets/Group1/Scripts/Player.cs
@@ -1,29 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(DistanceChecker))]
+[RequireComponent(typeof(DistanceToEnemies))]
 [RequireComponent(typeof(EnemyList))]
+[RequireComponent(typeof(Slowdown))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
     private EnemyList _enemies;
-    private DistanceChecker _checker;
+    private DistanceToEnemies _checker;
+    private Slowdown _slowdown;
 
     private void Awake()
     {
         _enemies = GetComponent<EnemyList>();
-        _checker = GetComponent<DistanceChecker>();
+        _checker = GetComponent<DistanceToEnemies>();
+        _slowdown = GetComponent<Slowdown>();
     }
 
     private void OnEnable()
     {
         _checker.EnemyChecked += OnAttack;
+        _slowdown.SlowdownDone += OnSlowdownDone;
     }
 
     private void OnDisable()
     {
         _checker.EnemyChecked -= OnAttack;
+        _slowdown.SlowdownDone -= OnSlowdownDone;
     }
 
     private void Update()
@@ -44,5 +49,10 @@ public class Player : MonoBehaviour
     {
         enemy.Die();
         _enemies.Enemies.Remove(enemy);
+    }
+
+    private void OnSlowdownDone()
+    {
+        _speed /= 2;
     }
 }
